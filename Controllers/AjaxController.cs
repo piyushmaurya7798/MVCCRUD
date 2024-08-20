b748ca48bb495e50d7746526d339037adb07c303
Windows <piyushmaurya7798@gmail.com>
2024-08-20 17:34:08 +0530

    Ajax More Functionality

--- Controllers/AjaxController.cs
+++ Controllers/AjaxController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using MVCCRUD.Context;
 using MVCCRUD.Models;
 
@@ -8,7 +9,7 @@ namespace MVCCRUD.Controllers
     {
         private readonly ApplicationDbContext db;
 
-        public AjaxController(ApplicationDbContext db) 
+        public AjaxController(ApplicationDbContext db)
         {
             this.db = db;
         }
@@ -22,6 +23,51 @@ namespace MVCCRUD.Controllers
             db.Emps.Add(e);
             db.SaveChanges();
             return Json("");
+        }
+
+
+        public IActionResult GetEmp()
+        {
+            var data = db.Emps.ToList();
+            return Json(data);
+        }
+        
+        public IActionResult DelEmp(int id)
+        {
+            var data = db.Emps.Find(id);
+            db.Emps.Remove(data);
+            db.SaveChanges();
+            return Json("");
+        }
+        [HttpPost]
+        public IActionResult updateEmp(Emp e)
+        {
+            //var data = db.Emps.Find(id);
+            db.Emps.Update(e);
+            db.SaveChanges();
+            return Json("");
+        }
+        
+        public IActionResult EditEmp(int id)
+        {
+            var data = db.Emps.Find(id);
+           
+            return Json(data);
+        }
+        [HttpPost]
+        public IActionResult Index(string Search)
+        {
+            if (Search.IsNullOrEmpty())
+            {
+                var data = db.Emps.ToList();
+                return Json(data);
+            }
+            else
+            {
+                var data = db.Emps.Where(a => a.Name.Contains(Search) || a.Email.Contains(Search)
+                            || a.Salary.ToString().Contains(Search)).ToList();
+                return Json(data);
+            }
         }
     }
 }
--- wwwroot/js/Demo.js
+++ wwwroot/js/Demo.js
@@ -1,5 +1,5 @@
 ﻿$(document).ready(function () {
-
+    GetEmp();
 })
 
 //$('#btn').click(function () {
@@ -9,9 +9,11 @@
 
 $('#ButtonModal').click(function () {
     $('#exampleModal').modal('show')
+    $('#hide').css("display", "none");
 })
 $('#closebtn').click(function () {
-    $('#exampleModal').modal('hide')
+    $('#exampleModal').modal('hide');
+    clear();
 })
 
 
@@ -23,7 +25,7 @@ function clear() {
 }
 
 $('#saveBtn').click(function () {
-
+   
     var obj = {
         name: $('#InName').val(),
         email: $('#InEmail').val(),
@@ -42,10 +44,128 @@ $('#saveBtn').click(function () {
             alert('Good');
             $('#exampleModal').modal('hide');
             clear();
+            GetEmp();
         },
         error: function () {
             alert('Not Good')
         }
 
     })
+})
+
+
+function deleteemp(id)
+{
+    if (confirm("Are you sure you want to delete"))
+    {
+        $.ajax({
+            url: '/Ajax/DelEmp?Id=' + id,
+            success: function () {
+                alert('Emp Deleted');
+                GetEmp();
+            },
+            error: function () {
+                alert('Not Successfull');
+            }
+        })
+    }
+
+}
+
+function EditEmp(id)
+{
+    $('#exampleModal').modal('show');
+    $('#UpdateBtn').css("display","block");
+    $('#saveBtn').css("display","none");
+    $('#hide').css("display","block");
+    
+    $.ajax({
+        url: '/Ajax/EditEmp?Id=' + id,
+        success: function (result) {
+            
+            $("#InName").val(result.name);
+            $("#ID").val(result.id);
+            $("#InEmail").val(result.email);
+            $("#InSalary").val(result.salary);
+
+        },
+        error: function () {
+            alert('Not Successfull');
+        }
+    })
+        
+}
+
+function GetEmp()
+{
+    $.ajax({
+        url: '/Ajax/GetEmp',
+        type: 'GET',
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function (result) {
+            obj = '';
+
+            $.each(result, function (index, item) {
+                obj += "<tr>"
+                obj += "<td>" + item.id + "</td>"
+                obj += "<td>" + item.name + "</td>"
+                obj += "<td>" + item.email + "</td>"
+                obj += "<td>" + item.salary + "</td>"
+                obj += "<td><input type='button' class='btn btn-danger' onclick='deleteemp("+item.id+")' value='Delete'></input></td>"
+                obj += "<td><input type='button' class='btn btn-secondary' onclick='EditEmp("+item.id+")' value='Edit'></input></td>"
+                obj += "</tr>"
+            });
+            $("#tablebody").html(obj);
+        },
+        error: function () {
+            alert('Not Good')
+        }
+    })
+}
+
+
+
+$('#UpdateBtn').click(function () {
+
+    var obj2 = $('#myform').serialize();
+    console.log(obj2);
+    $.ajax({
+        url: '/Ajax/updateEmp',
+        type: 'POST',
+        dataType: 'json',
+        contentType: 'application/x-www-form-urlencoded;charset=utf8',
+        data: obj2,
+        success: function () {
+            alert('Good');
+            $('#exampleModal').modal('hide');
+            clear();
+            GetEmp();
+        },
+        error: function () {
+            alert('Not Good');
+            clear();
+        }
+
+    })
+})
+
+
+$('#searchBtn').click(function () {
+    var sdata=$('searchdata').val();
+    $.ajax({
+        url: '/Ajax/Index?data=' + sdata,
+        type: 'POST',
+        dataType: 'json',
+        contentType: 'application/x-www-form-urlencoded;charset=utf8',
+        success: function () {
+          
+            //GetEmp();
+        },
+        error: function () {
+            alert('Not Good');
+            clear();
+        }
+
+    }) 
 })
